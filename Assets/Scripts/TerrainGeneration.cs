bd61c7239fe27f1ec7ad443e9a65543275fb790d
Peter Sparrow <peter@thesparrows.net>
2023-11-06 10:15:58 +0000

    Terrai gen v2

--- Assets/Scripts/TerrainGeneration.cs
+++ Assets/Scripts/TerrainGeneration.cs
@@ -14,7 +14,9 @@ public class TerrainGeneration : MonoBehaviour
     [SerializeField] private float scale1y;
     [SerializeField] private float scale2y;
     [SerializeField] private float scale3y;
-    void Update()
+
+
+    void Start()
     {
         Mesh mesh = new Mesh();
         List<Vector3> vertices = new List<Vector3>();
@@ -25,32 +27,29 @@ public class TerrainGeneration : MonoBehaviour
         {
             for (float z = 0; z < size; z += resolution)
             {
-                float y1 = Mathf.PerlinNoise(x *scale1,z*scale1)*scale1y;
-                float y2 = Mathf.PerlinNoise(x *scale2,z*scale2)*scale2y;
-                float y3 = Mathf.PerlinNoise(x *scale3,z*scale3)*scale3y;
-                float y = y1+y2+y3;
-                vertices.Add( new Vector3(x, y, z));
-            }
-        }
+                //add the verticies for the first triangle
+                vertices.Add(GetVertex(x+1,z));
+                vertices.Add(GetVertex(x,z+1));
+                vertices.Add(GetVertex(x,z));
+                //adds the first triangle
+                triangles.Add(vertices.Count-1);
+                triangles.Add(vertices.Count-2);
+                triangles.Add(vertices.Count-3);
 
-        //generates triangles
-        int num = (int) (size/resolution);
-        for (int z = 0; z < num-1; z ++)
-        {
-            for (int x = 0; x < num-1; x ++) 
-            {
-                //for now it simply generates A triangle will work on the BEST triangle later
-                triangles.Add(x + (z * num) +1);
-                triangles.Add(x + (z+1)*num);
-                triangles.Add(x + z * num);
 
-                triangles.Add(x + (z+1)*num +1);
-                triangles.Add(x + (z+1)*num);
-                triangles.Add(x + (z * num) +1);
+                //add the verticies for the second triangle
+                vertices.Add(GetVertex(x+1,z+1));
+                vertices.Add(GetVertex(x,z+1));
+                vertices.Add(GetVertex(x+1,z));
+                //adds the second triangle
+                triangles.Add(vertices.Count-1);
+                triangles.Add(vertices.Count-2);
+                triangles.Add(vertices.Count-3);
             }
         }
 
         //sets up the new mesh
+        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
         mesh.RecalculateNormals();
@@ -58,5 +57,15 @@ public class TerrainGeneration : MonoBehaviour
         //applies the new mesh to the componentes
         GetComponent<MeshFilter>().mesh = mesh;
         GetComponent<MeshCollider>().sharedMesh = mesh;
+    }
+
+    //returns a point in space based of 3 layered noise maps
+    private Vector3 GetVertex(float x, float z) 
+    {
+        float y1 = Mathf.PerlinNoise(x * scale1,z*scale1)*scale1y;
+        float y2 = Mathf.PerlinNoise(x * scale2,z*scale2)*scale2y;
+        float y3 = Mathf.PerlinNoise(x * scale3,z*scale3)*scale3y;
+        float y = y1+y2+y3;
+        return new Vector3(x, y, z);
     }
 }
